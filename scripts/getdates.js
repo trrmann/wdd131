aa36b9871a58407f48299e9b9eafc8bc46c43a01
trrmann <tmann@byupathway.edu>
2026-03-03 13:49:49 -0500

    fix copyright size and set limit to min only and no max size

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,4 @@
 const now = new Date();
-document.getElementById("currentyear").textContent = `\u00A9${now.getFullYear()} - `;
+document.getElementById("copyright").textContent = '\u00A9';
+document.getElementById("currentyear").textContent = `${now.getFullYear()} - `;
 document.getElementById("lastModified").textContent = `Last Modified:  ${document.lastModified}`;
